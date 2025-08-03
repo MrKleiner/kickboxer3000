6b222ac13b185a273e8740dc00c8bde52cc60eea
MrKleiner <37084600+MrKleiner@users.noreply.github.com>
2025-08-03 14:36:31 +0200

    use IPC reload instead of window.location.reload() on starting page

--- app/src/modules_c/starting_page/starting_page.js
+++ app/src/modules_c/starting_page/starting_page.js
@@ -1,7 +1,7 @@
 
 if(!window.kbmodules){window.kbmodules={}};
 if(!window.kbmodules.starting_page){window.kbmodules.starting_page={}};
-
+// const { ipcRenderer } = require('electron');
 
 window.kbmodules.starting_page.load = function(){};
 
@@ -11,7 +11,9 @@ window.kbmodules.starting_page.save_creds = function()
 	ksys.context.global.cache['vmix_ip'] = $('#welcome_enter_info [ip]').val();
 	ksys.context.global.cache['vmix_port'] = $('#welcome_enter_info [port]').val();
 	ksys.context.global.save();
-	window.location.reload();
+	// window.location.reload();
+	ksys.util.reload();
+
 }
 
 
