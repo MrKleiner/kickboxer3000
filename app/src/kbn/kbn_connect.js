8d89e154e815293f3960d57b21d6f371747ea393
MrKleiner <37084600+MrKleiner@users.noreply.github.com>
2025-09-23 19:56:40 +0200

    KBNC MAssive improvements

--- app/src/kbn/kbn_connect.js
+++ app/src/kbn/kbn_connect.js
@@ -27,7 +27,7 @@ const ConnectionAborted = class extends Error{
 	constructor(e){
 		super(e);
 		this.name = 'ConnectionAborted';
-		this.code = 'ERRCONABORT';
+		this.code = 'KB_ERRCONABORT';
 		if (Error.captureStackTrace){
 			Error.captureStackTrace(this, ConnectionAborted);
 		}
@@ -56,6 +56,39 @@ const consumableBuffer = class{
         self.buf = self.buf.slice(amount);
         return data
     }
+}
+
+
+const payloadStreamWrite = class{
+	CHUNK_SIZE = 8192;
+
+	constructor(payloadLength, dataIter){
+		const self = kbn_util.nprint(cls_pwnage.remap(this));
+
+		self.payloadLength = payloadLength;
+		self.srcBuf = null;
+		if (Buffer.isBuffer(dataIter)){
+			self.srcBuf = dataIter;
+			self.dataIter = self.iterBuf;
+		}else{
+			self.dataIter = dataIter;
+		}
+	}
+
+	*iterBuf(self){
+		let offs = 0;
+		while (true){
+			const chunk = self.srcBuf.slice(
+				offs,
+				offs + self.CHUNK_SIZE
+			);
+			if (!chunk.length){
+				break
+			}
+			yield chunk;
+			offs += self.CHUNK_SIZE;
+		}
+	}
 }
 
 
@@ -120,22 +153,67 @@ const KBNConnectMSG = class{
 		}
 
 		// const payloadBuf = Buffer.from(self.MSGData.payload || '');
-		const payloadBuf = (
-			Buffer.isBuffer(self.MSGData.payload) ?
-			self.MSGData.payload : Buffer.from(self.MSGData.payload || '')
-		)
+		// const payloadBuf = (
+		// 	Buffer.isBuffer(self.MSGData.payload) ?
+		// 	self.MSGData.payload : Buffer.from(self.MSGData.payload || '')
+		// )
 
-		self.MSGData.header.sys.payloadLen = payloadBuf.length;
+		let payloadBuf = null;
+
+		if (self.MSGData.payload instanceof payloadStreamWrite){
+			payloadBuf = payloadStream;
+		}else if (Buffer.isBuffer(self.MSGData.payload)){
+			payloadBuf = new payloadStreamWrite(
+				self.MSGData.payload.length,
+				self.MSGData.payload
+			);
+		}else{
+			try{
+				const buf = Buffer.from(self.MSGData.payload || '');
+				payloadBuf = new payloadStreamWrite(
+					buf.length,
+					buf
+				);
+			}catch{
+				const buf = Buffer.from(str(self.MSGData.payload));
+				payloadBuf = new payloadStreamWrite(
+					buf.length,
+					buf
+				);
+			}
+		}
+
+		// if (Buffer.isBuffer(self.MSGData.payload)){
+		// 	payloadBuf = self.MSGData.payload;
+		// }else{
+		// 	try{
+		// 		payloadBuf = Buffer.from(self.MSGData.payload || '');
+		// 	}catch{
+		// 		payloadBuf = Buffer.from(str(self.MSGData.payload));
+		// 	}
+		// }
+
+		self.MSGData.header.sys.payloadLen = payloadBuf.payloadLength;
 
 		const headerData = Buffer.from(
 			JSON.stringify(self.MSGData.header)
 		)
 
-		await self.sched.skt.write(
+		await self.sched.writeBytes(
 			kbn_util.toInt32(headerData.length)
 		)
-		await self.sched.skt.write(headerData);
-		await self.sched.skt.write(payloadBuf);
+		await self.sched.writeBytes(headerData);
+		for (const chunk of payloadBuf.iterBuf()){
+			await self.sched.writeBytes(chunk);
+		}
+		// await self.sched.writeBytes(payloadBuf);
+
+		// await self.sched.skt.write(
+		// 	kbn_util.toInt32(headerData.length)
+		// )
+		// await self.sched.skt.write(headerData);
+		// await self.sched.skt.write(payloadBuf);
+		
 
 		self.sendResolve();
 	}
@@ -165,14 +243,21 @@ const KBNConnectSocketSched = class{
 		}
 
 	*/
-	constructor(skt, msgExec){
+	constructor(skt, msgExec, callbacks=null){
 		const self = kbn_util.nprint(cls_pwnage.remap(this));
 
-		// The target socket
-		self.skt = skt;
+		// Callbacks
+		const onDeath = callbacks?.onDeath;
+		self.onDeath = function(...others){
+			onDeath?.(...others);
+			self.onDeath = function(){};
+		}
 
 		// Don't do shit when this is false
 		self.enabled = true;
+
+		// The target socket
+		self.skt = skt;
 
 		// Stupid shit
 		self.sktIter = self.skt[Symbol.asyncIterator]();
@@ -211,6 +296,23 @@ const KBNConnectSocketSched = class{
 		}
 	}
 
+	// Send bytes to the other side via self.skt
+	// Does a bunch of extra shit as opposed to skt.write,
+	// which, naturally, IS A FUCKING INDECISIVE TWAT
+	writeBytes(self, tgtBuf){
+		return new Promise(function(resolve, reject){
+			const sktInternalBufOk = self.skt.write(tgtBuf, 'utf8', function(err){
+				if (err){
+					reject(err);
+				}else{
+					resolve(sktInternalBufOk);
+				}
+			});
+		})
+	}
+
+	// Read from self.skt into self.TCPBufIn until it becomes
+	// longer than or equal to the amount of bytes requested
 	async readBytes(self, nBytes){
 		while (self.TCPBufIn.buf.length < nBytes){
 			const {value, done} = await self.sktIter.next();
@@ -224,6 +326,49 @@ const KBNConnectSocketSched = class{
 		}
 
 		return self.TCPBufIn.eraseRead(nBytes);
+	}
+
+	async *readBytesStream(self, nBytes){
+		if (nBytes <= 0){
+			yield Buffer.alloc(0);
+			return
+		}
+
+		let bytesRead = 0;
+		let value = self.TCPBufIn.eraseRead(nBytes);
+		let done = false;
+
+		while (true){
+			if ((bytesRead + value.length) > nBytes){
+				const dataEnd = nBytes - bytesRead;
+				self.TCPBufIn.write(
+					value.slice(dataEnd)
+				);
+				yield value.slice(
+					0,
+					dataEnd
+				)
+				break
+			}
+
+			bytesRead += value.length;
+
+			yield value
+
+			if (bytesRead >= nBytes){
+				return
+			}
+
+			const chunkData = await self.sktIter.next();
+			value = chunkData.value;
+			done = chunkData.done;
+
+			if (done){
+				throw new ConnectionAborted(
+					'Socket data iterator aborted'
+				);
+			}
+		}
 	}
 
 	async readHeader(self){
@@ -249,6 +394,7 @@ const KBNConnectSocketSched = class{
 
 		while (self.enabled){
 			try{
+				self.nprint('Awaiting message...');
 				// Header is always present regardless of context
 				const msgHeader = await self.readHeader();
 				self.nprint('Read header:', msgHeader);
@@ -257,9 +403,17 @@ const KBNConnectSocketSched = class{
 				const MSGID = msgHeader.sys.MSGID;
 
 				// Todo: Support streaming for potentially large payloads
-				const msgPayload = await self.readBytes(msgHeader.sys.payloadLen || 0);
+				// const msgPayload = await self.readBytes(msgHeader.sys.payloadLen || 0);
+				const msgPayload = Buffer.alloc(msgHeader.sys.payloadLen || 0);
+
+				let offs = 0;
+				for await (const chunk of self.readBytesStream(msgPayload.length)){
+					// self.nprint('Reading chunk:', chunk.length, [str(chunk).slice(0, 16)]);
+					offs += chunk.copy(msgPayload, offs);
+				}
 
 				if (msgHeader.sys.MSGType == 'exec'){
+					// todo: This looks APPALLING
 					(async function(){
 						return await self.msgExec({
 							'header': msgHeader.extra,
@@ -282,6 +436,7 @@ const KBNConnectSocketSched = class{
 				}
 
 				if (msgHeader.sys.MSGType == 'result'){
+					// todo: This shit is INDECISIVE
 					const targetMSG = self.MSGDictExec.get(MSGID);
 					if (targetMSG){
 						if (msgHeader.sys.errorData){
@@ -299,11 +454,15 @@ const KBNConnectSocketSched = class{
 				}
 
 			}catch(err){
-				self.nprint(err);
+				// Todo: what kind of exceptions can go here and what do they mean?
+				// self.nprint(`${err.prototype.constructor.name}`, err);
+				self.nerr(`>${err?.constructor?.name}<`, '\n', err);
 
 				if (err instanceof ConnectionAborted){
-					self.nprint('Client disconnect:', err);
+					self.nerr('Connection collapsed:', '\n', err);
 					self.rejectPending();
+					try{self.terminate()}catch{};
+					self.onDeath(err);
 					return
 				}
 			}
@@ -311,8 +470,14 @@ const KBNConnectSocketSched = class{
 	}
 
 	async writeSKTStream(self, msg){
-		await msg.send();
-		// self.nprint('Sent', msg.MSGData);
+		try{
+			await msg.send();
+		}catch(err){
+			self.nerr('Write ERROR:', '\n', err);
+			self.onDeath(err);
+			// todo: Don't give up so easily?
+			self.terminate();
+		}
 	}
 
 	// Schedule a message to be sent, which contains a result of an executed message
@@ -324,15 +489,17 @@ const KBNConnectSocketSched = class{
 					'MSGType': 'result',
 					'errorData': errorData,
 				},
-				'extra': resultData.header,
+				'extra': resultData?.header,
 			},
-			'payload': resultData.payload,
+			'payload': resultData?.payload,
 		})
 
 		// self.MSGDictResult.set(MSGID, msg);
 		self.MSGOutSched.push(msg);
 	}
 
+	// Scedule a message to be sent,
+	// which contains data to be executed by the other side
 	async schedMSGExec(self, MSGData){
 		const MSGID = self.lastCMDID++;
 
@@ -354,11 +521,25 @@ const KBNConnectSocketSched = class{
 
 	async terminate(self){
 		self.enabled = false;
+		self.onDeath(new ConnectionAborted(
+			'Socket force terminated'
+		));
 		await self.skt.destroy();
 		self.rejectPending();
 	}
 }
 
+
+
+const TESTCMDHandler = function(MSGData){
+	return {
+		'header': {
+			'payloadLengthEcho': MSGData.payload.length,
+			'headerEcho': MSGData?.header?.CMDID,
+		},
+		'payload': kbn_util.rnd_uuid(),
+	}
+}
 
 
 
@@ -375,6 +556,8 @@ const KBNConnectSocketServer = class{
 		    - Node
 		    - Connect
 	*/
+
+	static NPRINT_LEVEL = 0;
 
 	constructor(kbn){
 		const self = kbn_util.nprint(cls_pwnage.remap(this));
@@ -464,28 +647,65 @@ const KBNConnectSocketServer = class{
 	}
 
 	async msgInExec(self, MSGData){
-		self.nprint('TEST Client Message:', MSGData, [str(MSGData.payload).slice(0, 32)]);
+		self.nprintL(1,
+			'Client Message:', '\n',
+			MSGData?.payload?.slice?.(0, 16), '\n',
+			[str(MSGData.payload.slice(0, 32))]
+		);
 
-		self.clientSocketSched.schedMSGExec({
-			'header': kbn_util.rnd_uuid(),
-			'payload': 'SAMPLE',
-		});
-
-		// await kbn_util.sleep(15);
-
-		return {
-			'header': `R:${MSGData.header}`,
-			'payload': kbn_util.rnd_uuid(),
+		if ( (MSGData.header.CMDID != 'auth') && !self.clientSocketSched.authorized){
+			self.nprint('FATAL: Unauthorized access');
+			try{self.clientSocketSched.sched.terminate()}catch{};
+			return
 		}
+
+		if (MSGData.header.CMDID == 'auth'){
+			self.clientSocketSched.authorized = (
+				str(MSGData?.payload) == self.key
+			)
+
+			self.nprint([
+				'',
+				'======================',
+				`Auth result: ${self.clientSocketSched.authorized}`,
+				'======================',
+				'',
+			].join('\n'))
+
+			return {
+				'header': true,
+				'payload': true,
+			}
+		}
+
+		return await TESTCMDHandler(MSGData);
 	}
 
 	createClientConnection(self, skt){
-		self.clientSocketSched = new KBNConnectSocketSched(
+		if (self.clientSocketSched){
+			self.nprint('Client already connected. Denying');
+			try{skt.destroy()}catch{};
+			return
+		}
+
+		const sched = new KBNConnectSocketSched(
 			skt,
-			self.msgInExec
+			self.msgInExec,
+			{
+				'onDeath': function(err){
+					self.nprint('Current clientSocketSched is no longer with us');
+					try{self.clientSocketSched.skt.destroy()}catch{};
+					self.clientSocketSched = null;
+				},
+			}
 		);
 
-		self.clientSocketSched.readSKTStream();
+		self.clientSocketSched = {
+			'authorized': false,
+			'sched': sched,
+		};
+
+		self.clientSocketSched.sched.readSKTStream();
 
 		self.nprint('Client connected');
 	}
@@ -502,6 +722,7 @@ const KBNConnectSocketServer = class{
 
 		return new Promise(async function(resolve, reject){
 			let [serverExistsPromise, serverExistsResolve] = kbn_util.flatPromise();
+			let [clientConnectedPromise, clientConnectedResolve] = kbn_util.flatPromise();
 
 			while (self.enabled){
 				try{
